d86d19a5201fe5d758e37a7b994a20ef8085622d
최재강 <chl8273@naver.com>
2021-10-18 16:02:15 +0900

    Update tpu_inference_test.py

--- TPU/tpu_inference_test.py
+++ TPU/tpu_inference_test.py
@@ -27,14 +27,16 @@ assert re.search(r'gs://.+', BUCKET), 'For this part, you need a GCS bucket. Hea
 
 def deserialize_image_record(record):
     feature_map = {'image/encoded': tf.io.FixedLenFeature([], tf.string, ''),
-                  'image/class/label': tf.io.FixedLenFeature([1], tf.int64, -1)}
+                  'image/class/label': tf.io.FixedLenFeature([1], tf.int64, -1),
+                  'image/class/text': tf.io.FixedLenFeature([], tf.string, '')}
     obj = tf.io.parse_single_example(serialized=record, features=feature_map)
     imgdata = obj['image/encoded']
     label = tf.cast(obj['image/class/label'], tf.int32)   
-    return imgdata, label
+    label_text = tf.cast(obj['image/class/text'], tf.string)   
+    return imgdata, label, label_text
 
 def val_preprocessing(record):
-    imgdata, label = deserialize_image_record(record)
+    imgdata, label, label_text = deserialize_image_record(record)
     label -= 1
     
     image = tf.io.decode_jpeg(imgdata, channels=3, 
@@ -54,14 +56,13 @@ def val_preprocessing(record):
     new_width = tf.cast(tf.math.rint(width * scale), tf.int32)
     
     image = tf.image.resize(image, [new_height, new_width], method='bicubic')
-#     image = tf.image.resize_with_crop_or_pad(image, 224, 224)
-    
-    
+    image = tf.image.resize_with_crop_or_pad(image, 224, 224)
 
-#     label = tf.cast(label, tf.int32)
+    label_text = tf.cast(label_text, tf.int32)
+    label = tf.cast(label, tf.int32)
     image = resnet50.preprocess_input(image)
-#     image = tf.cast(image, tf.float32)
-    return image, label
+    image = tf.cast(image, tf.float32)
+    return image, label, label_text
 
 def get_dataset(batch_size, use_cache=False):
     data_dir = 'gs://jg-tpubucket/tf-record/validation-00000-of-00001'
