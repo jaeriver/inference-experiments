115ddfa92eeec8731b2a7c4e6914716ef1658aab
최재강 <chl8273@naver.com>
2022-03-21 23:08:06 +0900

    add parser

--- CPU/inference.py
+++ CPU/inference.py
@@ -44,8 +44,16 @@ models = {
 #     'mobilenet_v2':mobilenet_v2,
 #     'efficientnet_b7':efficientnet
 }
-model_type = "resnet50"
-batch_list = [1]
+parser = argparse.ArgumentParser()
+parser.add_argument('-m','--model_type', required=True)
+print(parser)
+parser.add_argument('-l', '--batch_list',
+                      nargs='+',
+                      required=True)
+
+model_type = parser.parse_args().model_type
+batch_list = parser.parse_args().batch_list
+
 
 def deserialize_image_record(record):
     feature_map = {'image/encoded': tf.io.FixedLenFeature([], tf.string, ''),
