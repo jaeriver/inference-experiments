365cf4fbf17839cc97778063cbce18b695a69fc9
최재강 <chl8273@naver.com>
2022-03-21 17:32:55 +0900

    test vgg16

--- GPU/inference.py
+++ GPU/inference.py
@@ -44,7 +44,7 @@ models = {
 #     'mobilenet_v2':mobilenet_v2,
 #     'efficientnet_b7':efficientnet
 }
-model_type = "resnet50"
+model_type = "vgg16"
 batch_list = [1]
 
 gpu_devices = tf.config.experimental.list_physical_devices('GPU')
