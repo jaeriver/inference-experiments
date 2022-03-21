b01d9291adfd6b6299e54945b347ec31fc522ec3
최재강 <chl8273@naver.com>
2022-03-21 17:20:06 +0900

    Update inference.py

--- GPU/inference.py
+++ GPU/inference.py
@@ -47,22 +47,8 @@ models = {
 model_type = "resnet50"
 batch_list = [1]
 
-os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-   tf.config.experimental.set_memory_growth(physical_devices[0], True)
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    try:
-        # Currently, memory growth needs to be the same across GPUs
-        print('test gpu')
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-    except RuntimeError as e:
-        # Memory growth must be set before GPUs have been initialized
-        print(e)
+gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+for device in gpu_devices: tf.config.experimental.set_memory_growth(device, True)
 
 def deserialize_image_record(record):
     feature_map = {'image/encoded': tf.io.FixedLenFeature([], tf.string, ''),
